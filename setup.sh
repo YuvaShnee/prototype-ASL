9c59d99980cfe5c6a877c026a80bbcd0ab17ce89
Yuva Shnee G <yuvashnee25@gmail.com>
2025-10-15 10:44:05 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx
+sudo apt-get install -y libgstreamer1.0-dev
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y libgstreamer-plugins-bad1.0-dev
+sudo apt-get install -y gstreamer1.0-plugins-base
+sudo apt-get install -y gstreamer1.0-plugins-good
+sudo apt-get install -y gstreamer1.0-plugins-bad
+sudo apt-get install -y gstreamer1.0-plugins-ugly
+sudo apt-get install -y gstreamer1.0-libav
+sudo apt-get install -y gstreamer1.0-tools
+sudo apt-get install -y gstreamer1.0-x
+sudo apt-get install -y gstreamer1.0-alsa
+sudo apt-get install -y gstreamer1.0-gl
+sudo apt-get install -y gstreamer1.0-gtk3
+sudo apt-get install -y gstreamer1.0-qt5
+sudo apt-get install -y gstreamer1.0-pulseaudio
